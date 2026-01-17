d93fa7589347c3b547cfd95d94d5c24db603192a
ono-teruya <27873650+teru01@users.noreply.github.com>
2026-01-17 10:48:40 +0900

    add layout

---
+++ dbrecord/layout.go
@@ -0,0 +1,56 @@
+package dbrecord
+
+import (
+	"github.com/teru01/simpledb-go/dbfile"
+	"github.com/teru01/simpledb-go/size"
+)
+
+// schemaのフィールドの配置情報
+type Layout struct {
+	schema   Schema
+	offsets  map[string]int
+	slotSize int
+}
+
+func NewLayout(schema Schema) *Layout {
+	layout := Layout{schema: schema}
+	pos := size.IntSize
+	offsets := make(map[string]int)
+	for _, field := range schema.fields {
+		offsets[field] = pos
+		pos += layout.LengthInBytes(field)
+	}
+	layout.slotSize = pos
+	return &layout
+}
+
+func NewLayoutFromOffsets(schema Schema, offsets map[string]int, slotSize int) *Layout {
+	return &Layout{
+		schema:   schema,
+		offsets:  offsets,
+		slotSize: slotSize,
+	}
+}
+
+func (l *Layout) Schema() Schema {
+	return l.schema
+}
+
+func (l *Layout) Offset(fieldName string) int {
+	return l.offsets[fieldName]
+}
+
+func (l *Layout) SlotSize() int {
+	return l.slotSize
+}
+
+// layout上でのfield valueのサイズ
+func (l *Layout) LengthInBytes(fieldName string) int {
+	switch l.schema.FieldType(fieldName) {
+	case FieldTypeInt:
+		return size.IntSize
+	case FieldTypeString:
+		return dbfile.MaxStringLengthOnPage(l.schema.Length(fieldName))
+	}
+	return 0
+}
