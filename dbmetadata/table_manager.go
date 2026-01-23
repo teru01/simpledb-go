a3e49161b6dc4712a79fd319aa356fbb653b28a9
ono-teruya <27873650+teru01@users.noreply.github.com>
2026-01-23 21:46:42 +0900

    add table manager

---
+++ dbmetadata/table_manager.go
@@ -0,0 +1,159 @@
+package dbmetadata
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/teru01/simpledb-go/dbrecord"
+	"github.com/teru01/simpledb-go/dbtx"
+)
+
+const MaxNameLength = 16
+
+type TableManager struct {
+	tableCatalogLayout *dbrecord.Layout
+	fieldCatalogLayout *dbrecord.Layout
+}
+
+func NewTableManager(isNew bool, tx *dbtx.Transaction) *TableManager {
+	tableCatalogSchema := dbrecord.NewSchema()
+	tableCatalogSchema.AddStringField("tablename", MaxNameLength)
+	tableCatalogSchema.AddIntField("slotsize")
+	tableCatalogLayout := dbrecord.NewLayout(tableCatalogSchema)
+
+	fieldCatalogSchema := dbrecord.NewSchema()
+	fieldCatalogSchema.AddStringField("tablename", MaxNameLength)
+	fieldCatalogSchema.AddStringField("fieldname", MaxNameLength)
+	fieldCatalogSchema.AddIntField("type")
+	fieldCatalogSchema.AddIntField("length")
+	fieldCatalogSchema.AddIntField("offset")
+	fieldCatalogLayout := dbrecord.NewLayout(fieldCatalogSchema)
+
+	return &TableManager{
+		tableCatalogLayout: tableCatalogLayout,
+		fieldCatalogLayout: fieldCatalogLayout,
+	}
+}
+
+func (t *TableManager) CreateTable(ctx context.Context, tableName string, schema *dbrecord.Schema, tx *dbtx.Transaction) error {
+	layout := dbrecord.NewLayout(schema)
+
+	tableCatlog, err := dbrecord.NewTableScan(ctx, tx, "table_catalog", layout)
+	if err != nil {
+		return fmt.Errorf("create table scan for table_catalog when creating %q: %w", tableName, err)
+	}
+	if err := tableCatlog.Insert(ctx); err != nil {
+		return fmt.Errorf("insert to table_catalog for %q: %w", tableName, err)
+	}
+	if err := tableCatlog.SetString(ctx, "tablename", tableName); err != nil {
+		return fmt.Errorf("set tablename for %q: %w", tableName, err)
+	}
+	if err := tableCatlog.SetInt(ctx, "slotsize", layout.SlotSize()); err != nil {
+		return fmt.Errorf("set slotsize for %q: %w", tableName, err)
+	}
+	if err := tableCatlog.Close(); err != nil {
+		return fmt.Errorf("close table scan for table_catalog when creating %q: %w", tableName, err)
+	}
+	fieldCatlog, err := dbrecord.NewTableScan(ctx, tx, "field_catalog", layout)
+	if err != nil {
+		return fmt.Errorf("create table scan for field_catalog when creating %q: %w", tableName, err)
+	}
+	for _, fieldName := range schema.Fields() {
+		if err := fieldCatlog.SetString(ctx, "tablename", tableName); err != nil {
+			return fmt.Errorf("set tablename for %q: %w", tableName, err)
+		}
+		if err := fieldCatlog.SetString(ctx, "fieldname", fieldName); err != nil {
+			return fmt.Errorf("set fieldname for %q: %w", tableName, err)
+		}
+		if err := fieldCatlog.SetInt(ctx, "type", schema.FieldType(fieldName)); err != nil {
+			return fmt.Errorf("set type for %q: %w", tableName, err)
+		}
+		if err := fieldCatlog.SetInt(ctx, "length", schema.Length(fieldName)); err != nil {
+			return fmt.Errorf("set length for %q: %w", tableName, err)
+		}
+		if err := fieldCatlog.SetInt(ctx, "offset", layout.Offset(fieldName)); err != nil {
+			return fmt.Errorf("set offset for %q: %w", tableName, err)
+		}
+	}
+	if err := fieldCatlog.Close(); err != nil {
+		return fmt.Errorf("close table scan for field_catalog when creating %q: %w", tableName, err)
+	}
+	return nil
+}
+
+func (t *TableManager) GetLayout(ctx context.Context, tableName string, tx *dbtx.Transaction) (*dbrecord.Layout, error) {
+	tableCatlog, err := dbrecord.NewTableScan(ctx, tx, "table_catalog", t.tableCatalogLayout)
+	if err != nil {
+		return nil, fmt.Errorf("create table scan: %w", err)
+	}
+	slotSize := -1
+	for {
+		next, err := tableCatlog.Next(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("go next for %q: %w", "table_catalog", err)
+		}
+		if !next {
+			if err := tableCatlog.Close(); err != nil {
+				return nil, fmt.Errorf("close table_catalog: %w", err)
+			}
+			break
+		}
+		tableNameValue, err := tableCatlog.GetString(ctx, "tablename")
+		if err != nil {
+			return nil, fmt.Errorf("get tablename: %w", err)
+		}
+		if tableNameValue == tableName {
+			ss, err := tableCatlog.GetInt(ctx, "slotsize")
+			if err != nil {
+				return nil, fmt.Errorf("get slotsize: %w", err)
+			}
+			slotSize = ss
+		}
+	}
+
+	schema := dbrecord.NewSchema()
+
+	fieldCatlog, err := dbrecord.NewTableScan(ctx, tx, "field_catalog", t.fieldCatalogLayout)
+	if err != nil {
+		return nil, fmt.Errorf("create table scan: %w", err)
+	}
+
+	offsets := make(map[string]int)
+	for {
+		next, err := fieldCatlog.Next(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("go next for %q: %w", "field_catalog", err)
+		}
+		if !next {
+			if err := fieldCatlog.Close(); err != nil {
+				return nil, fmt.Errorf("close field_catalog: %w", err)
+			}
+			break
+		}
+		tableNameValue, err := fieldCatlog.GetString(ctx, "tablename")
+		if err != nil {
+			return nil, fmt.Errorf("get tablename: %w", err)
+		}
+		if tableNameValue == tableName {
+			fieldName, err := fieldCatlog.GetString(ctx, "fieldname")
+			if err != nil {
+				return nil, fmt.Errorf("get fieldname: %w", err)
+			}
+			fieldType, err := fieldCatlog.GetInt(ctx, "type")
+			if err != nil {
+				return nil, fmt.Errorf("get type: %w", err)
+			}
+			length, err := fieldCatlog.GetInt(ctx, "length")
+			if err != nil {
+				return nil, fmt.Errorf("get length: %w", err)
+			}
+			offset, err := fieldCatlog.GetInt(ctx, "offset")
+			if err != nil {
+				return nil, fmt.Errorf("get offset: %w", err)
+			}
+			schema.AddField(fieldName, fieldType, length)
+			offsets[fieldName] = offset
+		}
+	}
+	return dbrecord.NewLayoutFromOffsets(schema, offsets, slotSize), nil
+}
