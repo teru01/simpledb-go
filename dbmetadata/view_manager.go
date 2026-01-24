33c853dd212ca4370c9219ed3c505847457a0fdd
ono-teruya <27873650+teru01@users.noreply.github.com>
2026-01-24 12:00:09 +0900

    add view manager

---
+++ dbmetadata/view_manager.go
@@ -0,0 +1,93 @@
+package dbmetadata
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/teru01/simpledb-go/dbrecord"
+	"github.com/teru01/simpledb-go/dbtx"
+)
+
+const (
+	MaxViewDefinitionLength = 100
+	ViewCatalogTableName    = "view_catalog"
+)
+
+type ViewManager struct {
+	table_manager *TableManager
+}
+
+func NewViewManager(ctx context.Context, isNew bool, table_manager *TableManager, tx *dbtx.Transaction) (*ViewManager, error) {
+	if isNew {
+		schema := dbrecord.NewSchema()
+		schema.AddStringField("viewname", MaxNameLength)
+		schema.AddStringField("viewdef", MaxViewDefinitionLength)
+		if err := table_manager.CreateTable(ctx, ViewCatalogTableName, schema, tx); err != nil {
+			return nil, fmt.Errorf("init view manager: %w", err)
+		}
+	}
+	return &ViewManager{
+		table_manager: table_manager,
+	}, nil
+}
+
+func (v *ViewManager) CreateView(ctx context.Context, viewName string, viewDef string, tx *dbtx.Transaction) error {
+	layout, err := v.table_manager.GetLayout(ctx, ViewCatalogTableName, tx)
+	if err != nil {
+		return fmt.Errorf("get layout before creating view %q: %w", viewName, err)
+	}
+	ts, err := dbrecord.NewTableScan(ctx, tx, ViewCatalogTableName, layout)
+	if err != nil {
+		return fmt.Errorf("create table scan for view_catalog when creating view %q: %w", viewName, err)
+	}
+	if err := ts.Insert(ctx); err != nil {
+		return fmt.Errorf("insert to view_catalog for %q: %w", viewName, err)
+	}
+	if err := ts.SetString(ctx, "viewname", viewName); err != nil {
+		return fmt.Errorf("set viewname for %q: %w", viewName, err)
+	}
+	if err := ts.SetString(ctx, "viewdef", viewDef); err != nil {
+		return fmt.Errorf("set viewdef for %q: %w", viewName, err)
+	}
+	if err := ts.Close(); err != nil {
+		return fmt.Errorf("close table scan for view_catalog when creating view %q: %w", viewName, err)
+	}
+	return nil
+}
+
+func (v *ViewManager) GetViewDef(ctx context.Context, viewName string, tx *dbtx.Transaction) (string, error) {
+	layout, err := v.table_manager.GetLayout(ctx, ViewCatalogTableName, tx)
+	if err != nil {
+		return "", fmt.Errorf("get layout before get view %q: %w", viewName, err)
+	}
+	ts, err := dbrecord.NewTableScan(ctx, tx, ViewCatalogTableName, layout)
+	if err != nil {
+		return "", fmt.Errorf("create table scan for view_catalog when get view %q: %w", viewName, err)
+	}
+	viewDef := ""
+	for {
+		next, err := ts.Next(ctx)
+		if err != nil {
+			return "", fmt.Errorf("go next for %q: %w", "view_catalog", err)
+		}
+		if !next {
+			break
+		}
+		viewNameValue, err := ts.GetString(ctx, "viewname")
+		if err != nil {
+			return "", fmt.Errorf("get viewname value: %w", err)
+		}
+		if viewNameValue == viewName {
+			viewDefValue, err := ts.GetString(ctx, "viewdef")
+			if err != nil {
+				return "", fmt.Errorf("get viewdef value: %w", err)
+			}
+			viewDef = viewDefValue
+			break
+		}
+	}
+	if err := ts.Close(); err != nil {
+		return "", fmt.Errorf("close view_catalog: %w", err)
+	}
+	return viewDef, nil
+}
