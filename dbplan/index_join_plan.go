a15fbdd0ec00f869405355a78e4266c409298d48
ono-teruya <27873650+teru01@users.noreply.github.com>
2026-02-22 18:02:07 +0900

    add index join scan

--- dbplan/index_join_plan.go
+++ dbplan/index_join_plan.go
@@ -1,3 +1,44 @@
 package dbplan
 
-// type IndexJoinPlan
+import (
+	"context"
+	"fmt"
+
+	"github.com/teru01/simpledb-go/dbmetadata"
+	"github.com/teru01/simpledb-go/dbquery"
+	"github.com/teru01/simpledb-go/dbrecord"
+)
+
+type IndexJoinPlan struct {
+	p1, p2    dbquery.Plan
+	indexInfo *dbmetadata.IndexInfo
+	joinField string
+	schema    *dbrecord.Schema
+}
+
+func NewIndexJoinPlan(p1 dbquery.Plan, p2 dbquery.Plan, indexInfo *dbmetadata.IndexInfo, joinField string) *IndexJoinPlan {
+	schema := dbrecord.NewSchema()
+	schema.AddAll(p1.Schema())
+	schema.AddAll(p2.Schema())
+	return &IndexJoinPlan{p1: p1, p2: p2, indexInfo: indexInfo, joinField: joinField, schema: schema}
+}
+
+func (p *IndexJoinPlan) Open(ctx context.Context) (dbquery.Scan, error) {
+	s1, err := p.p1.Open(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("open plan1: %w", err)
+	}
+	s2Opened, err := p.p2.Open(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("open plan2: %w", err)
+	}
+	s2, ok := s2Opened.(*dbrecord.TableScan)
+	if !ok {
+		return nil, fmt.Errorf("s2 must be table scan: %w", err)
+	}
+	idx, err := p.indexInfo.Open(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("open index: %w", err)
+	}
+	return dbquery.NewIndexJoinScan(ctx, s1, idx, p.joinField, s2)
+}
---
+++ dbquery/index_join_scan.go
@@ -0,0 +1,105 @@
+package dbquery
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/teru01/simpledb-go/dbconstant"
+	"github.com/teru01/simpledb-go/dbindex"
+	"github.com/teru01/simpledb-go/dbrecord"
+)
+
+type IndexJoinScan struct {
+	lhs       Scan
+	index     dbindex.Index
+	joinField string
+	// インデックスによる検索対象
+	rhs *dbrecord.TableScan
+}
+
+func NewIndexJoinScan(ctx context.Context, lhs Scan, index dbindex.Index, joinField string, rhs *dbrecord.TableScan) (*IndexJoinScan, error) {
+	return &IndexJoinScan{lhs: lhs, index: index, joinField: joinField, rhs: rhs}, nil
+}
+
+func (s *IndexJoinScan) SetStateToBeforeFirst(ctx context.Context) error {
+	if err := s.lhs.SetStateToBeforeFirst(ctx); err != nil {
+		return fmt.Errorf("set state to before first: %w", err)
+	}
+	if _, err := s.lhs.Next(ctx); err != nil {
+		return fmt.Errorf("next: %w", err)
+	}
+	return s.ResetIndex(ctx)
+}
+
+// rhsを検索するインデックスのカーソルを先頭に戻す
+func (s *IndexJoinScan) ResetIndex(ctx context.Context) error {
+	searchKey, err := s.lhs.GetValue(ctx, s.joinField)
+	if err != nil {
+		return fmt.Errorf("get value: %w", err)
+	}
+	return s.index.BeforeFirst(ctx, searchKey)
+}
+
+func (s *IndexJoinScan) Next(ctx context.Context) (bool, error) {
+	for {
+		ok, err := s.index.Next(ctx)
+		if err != nil {
+			return false, fmt.Errorf("next index: %w", err)
+		}
+		if ok {
+			rid, err := s.index.GetDataRID(ctx)
+			if err != nil {
+				return false, fmt.Errorf("get data rid: %w", err)
+			}
+			if err := s.rhs.MoveToRID(ctx, *rid); err != nil {
+				return false, fmt.Errorf("move to %q: %w", rid, err)
+			}
+			return true, nil
+		}
+		exists, err := s.lhs.Next(ctx)
+		if err != nil {
+			return false, fmt.Errorf("next: %w", err)
+		}
+		if !exists {
+			return false, nil
+		}
+		if err := s.ResetIndex(ctx); err != nil {
+			return false, fmt.Errorf("reset index: %w", err)
+		}
+	}
+}
+
+func (s *IndexJoinScan) GetInt(ctx context.Context, fieldName string) (int, error) {
+	if s.rhs.HasField(fieldName) {
+		return s.rhs.GetInt(ctx, fieldName)
+	}
+	return s.lhs.GetInt(ctx, fieldName)
+}
+
+func (s *IndexJoinScan) GetString(ctx context.Context, fieldName string) (string, error) {
+	if s.rhs.HasField(fieldName) {
+		return s.rhs.GetString(ctx, fieldName)
+	}
+	return s.lhs.GetString(ctx, fieldName)
+}
+
+func (s *IndexJoinScan) GetValue(ctx context.Context, fieldName string) (dbconstant.Constant, error) {
+	if s.rhs.HasField(fieldName) {
+		return s.rhs.GetValue(ctx, fieldName)
+	}
+	return s.lhs.GetValue(ctx, fieldName)
+}
+
+func (s *IndexJoinScan) HasField(fieldName string) bool {
+	return s.rhs.HasField(fieldName) || s.lhs.HasField(fieldName)
+}
+
+func (s *IndexJoinScan) Close(ctx context.Context) error {
+	if err := s.index.Close(ctx); err != nil {
+		return fmt.Errorf("close index: %w", err)
+	}
+	if err := s.rhs.Close(ctx); err != nil {
+		return fmt.Errorf("close rhs: %w", err)
+	}
+	return s.lhs.Close(ctx)
+}
